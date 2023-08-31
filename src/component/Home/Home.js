8e56d37513fdaa51cc55296de6c04e3173f52b25
Kaspars Revins <okoikwedced@gmail.com>
2023-08-31 12:59:46 +0100

    Recreated one to one in react

---
+++ src/component/Home/Home.js
@@ -0,0 +1,16 @@
+import Footer from "../Footer/Footer"
+import Header from "../Header/Header"
+import Hero from "../Hero/Hero"
+import IconSection from "../IconSection/IconSection"
+
+const Home = () => {
+    return (
+        <div>
+            <Header />
+            <Hero />
+            <IconSection />
+            <Footer />
+        </div>
+    )
+}
+export default Home
--- src/index.js
+++ src/index.js
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import M from 'materialize-css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   var elems = document.querySelectorAll('.sidenav');
+//   var instances = M.Sidenav.init(elems, options);
+// });
+
 root.render(
   <React.StrictMode>
     <App />
